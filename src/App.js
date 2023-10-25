d8152211a28599857f6ce0cfcf0a37695ee972a4
NADA-samadi1 <nadasamadi20@gmail.com>
2023-10-25 11:56:29 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+
+
+
+
+      <h1>   Titre 1</h1>
+
+      <p>     Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas excepturi aliquam iste quibusdam? Rerum laboriosam sed, doloremque at quae nostrum aliquam commodi sequi neque accusantium voluptatum deleniti autem quis recusandae.</p>
+    </div>
+  );
+}
+
+export default App;
